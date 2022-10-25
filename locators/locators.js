986ce6eddaf93313531e01a467694cdb07252c5a
Shahid Qureshi <qureshi.shahid7777@gmail.com>
2022-10-25 16:54:32 +0530

    mocha-flow completed

---
+++ locators/locators.js
@@ -0,0 +1,13 @@
+module.exports = {
+  LinkedIn: {
+    joinNowBtnLabel: "a.nav__button-tertiary",
+    signInBtnLabel: "a.nav__button-secondary",
+    mainHeading: "h1.main-heading",
+    emailPhoneNumberInputLabel: '.input__label[for="session_key"]',
+    passwordLabel: '.input__label[for="session_password"]',
+    forgetPasswordLabel: "a.sign-in-form__forgot-password-link",
+    continueBtn: "#join-form-submit",
+    errorlabels: ".artdeco-inline-feedback__message",
+    InputField: (name) => `.input__input[name=${name}]`,
+  },
+};
---
+++ test/homepage.test.js
@@ -0,0 +1,84 @@
+const selector = require("../locators/locators");
+
+describe("Validate LinkedIn Page", () => {
+  let page, elementName;
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://linkedin.com/");
+    await page.setViewport({ width: 1300, height: 768 });
+  });
+  afterEach(async () => {
+    await page.close();
+  });
+  it("Validate page elements", async () => {
+    elementName = await page.$eval(
+      selector.LinkedIn.joinNowBtnLabel,
+      (el) => el.innerHTML
+    );
+    expect(elementName.replace(/\n/g, "").trim()).to.equal("Join now");
+    elementName = await page.$eval(
+      selector.LinkedIn.signInBtnLabel,
+      (el) => el.innerHTML
+    );
+    expect(elementName.replace(/\n/g, "").trim()).to.equal("Sign in");
+    elementName = await page.$eval(
+      selector.LinkedIn.mainHeading,
+      (el) => el.innerHTML
+    );
+    expect(elementName.replace(/\n/g, "").trim()).to.equal(
+      "Welcome to your professional community"
+    );
+    elementName = await page.$eval(
+      selector.LinkedIn.emailPhoneNumberInputLabel,
+      (el) => el.innerHTML
+    );
+    expect(elementName.replace(/\n/g, "").trim()).to.equal(
+      "Email or phone number"
+    );
+    elementName = await page.$eval(
+      selector.LinkedIn.passwordLabel,
+      (el) => el.innerHTML
+    );
+    expect(elementName.replace(/\n/g, "").trim()).to.equal("Password");
+    elementName = await page.$eval(
+      selector.LinkedIn.forgetPasswordLabel,
+      (el) => el.innerHTML
+    );
+    expect(elementName.replace(/\n/g, "").trim()).to.equal("Forgot password?");
+  });
+
+  it("Sign up - Without email and password", async () => {
+    const pageTarget = page.target();
+    await page.click(selector.LinkedIn.joinNowBtnLabel);
+    await page.click(selector.LinkedIn.continueBtn);
+    await page.waitForSelector(selector.LinkedIn.errorlabels);
+    const options = await page.$$eval(
+      selector.LinkedIn.errorlabels,
+      (options) => options.map((option) => option.innerHTML)
+    );
+
+    expect(options[0].replace(/\n/g, "").trim()).to.equal(
+      "Please enter your email address or mobile number."
+    );
+    expect(options[1].replace(/\n/g, "").trim()).to.equal(
+      "Please enter your password."
+    );
+  });
+
+  it("Sign up - With email and password", async () => {
+    await page.click(selector.LinkedIn.joinNowBtnLabel);
+    await page.type(
+      selector.LinkedIn.InputField("email-or-phone"),
+      "testing0909876@gmail.com"
+    );
+    await page.type(
+      selector.LinkedIn.InputField("password"),
+      "0909876learning"
+    );
+    await page.click(selector.LinkedIn.continueBtn);
+    await page.waitForSelector(selector.LinkedIn.InputField("first-name"));
+    await page.type(selector.LinkedIn.InputField("first-name"), "learning");
+    await page.type(selector.LinkedIn.InputField("last-name"), "tester");
+    await page.click(selector.LinkedIn.continueBtn);
+  });
+});
